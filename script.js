d55314adc077fec82df20ebcd048da9d9b79481c
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-06 12:31:33 -0400

    user is able to place marker on board

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
+const gameBoard = document.getElementById('gameBoard');
+let board = ['', '', '', '', '', '', '', '', ''];
+let playerTurn = 'X';
+
 const GameBoard = (() => {
-    let board = ['X', 'X', 'O', '', '', 'O', 'O', 'X', ''];
+
 
     const displayBoard = () => {
-        const gameBoard = document.getElementById('gameBoard');
         for (let i = 0; i < gameBoard.children.length; i++) {
             gameBoard.children[i].innerText = board[i];
         }
     }
 
-    return {displayBoard};
+    const updateBoard = (newBoard) => {
+        board = newBoard.slice(0);
+    }
+
+    const removeHover = (square) => {
+        square.classList.remove('hover');
+    }
+
+    const addHover = (square) => {
+        square.classList.add('hover');
+    }
+
+    const placeMarker = () => {
+        const squares = document.querySelectorAll('.square');
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].onclick = function(e) {
+                let newBoard = board.slice(0);
+                newBoard[i] = playerTurn;
+                GameBoard.updateBoard(newBoard);
+                removeHover(squares[i]);
+                playerTurn = Game.switchTurns();
+                GameBoard.displayBoard();
+            }
+        }
+    }
+
+    return {displayBoard, board, removeHover, addHover, updateBoard, placeMarker};
 })();
 
-GameBoard.displayBoard();
+const Player = (name) => {
+
+}
+
+const Game = (() => {
+
+    const switchTurns = () => {
+        return (playerTurn === 'X' ? 'O':'X');
+    }
+
+    const startGame = () => {
+        GameBoard.updateBoard(['', '', '', '', '', '', '', '', '']);
+        for (let i = 0; i < gameBoard.children.length; i++) {
+            GameBoard.addHover(gameBoard.children[i]);
+        }
+        GameBoard.displayBoard();
+        playerTurn = 'X';
+    }
+
+    return {playerTurn, switchTurns, startGame};
+})()
+
+document.getElementById('start').addEventListener('click', Game.startGame);
+gameBoard.addEventListener('mouseover', GameBoard.placeMarker);
