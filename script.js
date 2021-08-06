d063c89be34da768d709fbf4913c8b19258a82bb
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-06 11:13:55 -0400

    implemented display board function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const GameBoard = () => {
-    let board = ['', '', '', '', '', '', '', '', ''];
+const GameBoard = (() => {
+    let board = ['X', 'X', 'O', '', '', 'O', 'O', 'X', ''];
 
     const displayBoard = () => {
-        
+        const gameBoard = document.getElementById('gameBoard');
+        for (let i = 0; i < gameBoard.children.length; i++) {
+            gameBoard.children[i].innerText = board[i];
+        }
     }
-}
+
+    return {displayBoard};
+})();
+
+GameBoard.displayBoard();
